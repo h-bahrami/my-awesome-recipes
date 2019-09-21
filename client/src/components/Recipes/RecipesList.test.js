320ea1c937f9376ae9219507bfd937cc7d8af8c5
h-bahrami <h.bahrami@live.com>
2019-09-21 11:45:52 +0200

    fix tests

--- client/src/components/Recipes/RecipesList.test.js
+++ client/src/components/Recipes/RecipesList.test.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { mount, shallow } from 'enzyme';
 import ReactDOM from 'react-dom';
-import Recipe from './Recipe';
+import RecipesList from './RecipesList';
 import renderer from 'react-test-renderer';
 
-const recipe = {
+
+const recipes = [{
     id: "e941dcb0-d7de-11e9-a822-0b6bc7db7f51",
     name: "Pizza",
     recordTime: "",
@@ -13,16 +14,46 @@ const recipe = {
     people: 3,
     ingredients: ["abc", "def"],
     instructions: "abcdefghijk"
-}
+},
+{
+    id: "706cc1f3-3c23-4eff-b02d-cf7764596d71",
+    name: "Bitterballen",
+    recordTime: "",
+    image: "",
+    vegeterian: false,
+    people: 3,
+    ingredients: ["abc", "def"],
+    instructions: "abcdefghijk"
+}]
 
 describe('Testing Recipes Component', () => {
+
+    const selectRecipe = jest.fn();
+    const updateRecipe = jest.fn();
+    const deleteRecipe = jest.fn();
+    const fetchRecipes = jest.fn();
+    const newRecipe = jest.fn();
+
+    const props = {
+        items: recipes,
+        loading: false,
+        selectedRecipe: null,
+        error: '',
+        selectRecipe,
+        updateRecipe,
+        deleteRecipe,
+        fetchRecipes,
+        newRecipe
+    };
+
+
     it('renders without crashing', () => {
-        shallow(<Recipe recipe={recipe} update={() => { }} />);
+        shallow(<RecipesList {...props} />);
     });
 
     it('snapshot test', () => {
         const tree = renderer
-            .create(<Recipe recipe={recipe} update={() => { }} />)
+            .create(<RecipesList {...props} />)
             .toJSON();
         expect(tree).toMatchSnapshot();
     });
